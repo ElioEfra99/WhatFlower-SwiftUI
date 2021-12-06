ca9e4fe82c6c659c1c5ab78303dffd4e75cea55e
Eliu Diaz <eedb_abcd_1234567890@ymail.com>
2021-12-06 17:18:21 -0600

    Fixing position of dismiss and favorite buttons

--- WhatFlower SwiftUI/View/FlowerResultView.swift
+++ WhatFlower SwiftUI/View/FlowerResultView.swift
@@ -13,63 +13,62 @@ struct FlowerResultView: View {
     
     var body: some View {
         GeometryReader { proxy in
-            ZStack {
-                // TODO: Add remaining pieces of UI
-                    VStack {
-                        ScrollView{
-                            AsyncImage(url: flowerObject.imageURL) { image in
-                                image
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                    .frame(width: proxy.size.width, height: 350)
-                                    .clipped()
-                                    .border(.red)
-                                    
-                                    
-                            } placeholder: {
-                                Image(systemName: "photo.circle.fill")
-                                    .resizable()
-                                    .frame(width: 300, height: 300)
-                            }
-                            VStack(alignment: .leading) {
-                                Text(flowerObject.title)
-                                    .font(.system(size: 24, weight: .medium))
-                                Text(flowerObject.extract)
-                            }
-                            .padding(.horizontal)
+            // TODO: Add remaining pieces of UI
+            VStack {
+                ScrollView{
+                    ZStack {
+                        AsyncImage(url: flowerObject.imageURL) { image in
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: proxy.size.width, height: 350)
+                                .clipped()
+                        } placeholder: {
+                            Image(systemName: "photo.circle.fill")
+                                .resizable()
+                                .frame(width: 300, height: 300)
                         }
                         
-                    }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-                    .ignoresSafeArea(edges: .top)
-                    .border(.green)
-
-                
-                VStack {
-                    HStack {
-                        Button {
-                            presentationMode.wrappedValue.dismiss()
-                        } label: {
-                            Image(systemName: "xmark.circle")
-                                .resizable()
-                                .frame(width: 25, height: 25)
-                                .foregroundColor(.white)
+                        VStack {
+                            HStack {
+                                Button {
+                                    presentationMode.wrappedValue.dismiss()
+                                } label: {
+                                    Image(systemName: "xmark.circle")
+                                        .resizable()
+                                        .frame(width: 25, height: 25)
+                                        .foregroundColor(.white)
+                                }
+                                Spacer()
+                                Button {
+                                    print("Favorite")
+                                } label: {
+                                    Image(systemName: "heart")
+                                        .resizable()
+                                        .frame(width: 25, height: 22)
+                                        .foregroundColor(.white)
+                                }
+                            }
+                            .padding(.top, 50)
+                            .padding(.horizontal)
+                            Spacer()
                         }
-                        Spacer()
-                        Button {
-                            print("Favorite")
-                        } label: {
-                            Image(systemName: "heart")
-                                .resizable()
-                                .frame(width: 25, height: 22)
-                                .foregroundColor(.white)
-                        }
+                        
+                    }.border(.red)
+                    
+                    VStack(alignment: .leading) {
+                        Text(flowerObject.title)
+                            .font(.system(size: 24, weight: .medium))
+                            .padding(.top, 16)
+                        Text(flowerObject.extract)
+                            .padding(.top, 16)
                     }
-                    .padding(.top, 16)
                     .padding(.horizontal)
-                }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-            }
+                }
+                
+            }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+            .ignoresSafeArea(edges: .top)
         }
-        
     }
 }
 
